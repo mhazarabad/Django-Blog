b3a28e728e279286d8b8ce78d905dec6f200b0ea
M Hadi Azarabad <mhazarabad@gmail.com>
2024-01-22 20:56:45 +0100

    back links fixed

--- django_blog/urls.py
+++ django_blog/urls.py
@@ -20,6 +20,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', include ('blog.urls')),
     path('admin/', admin.site.urls),
+    path('', include ('blog.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
